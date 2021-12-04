1c3f4dd88c8417ca7d4e6ecc56a59a5f5254130c
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-12-04 14:48:08 +0530

    UI Done

---
+++ musicdata.js
@@ -0,0 +1,38 @@
+// Music Data
+var Music = [
+    {
+        name: "song1",
+        url: "songurl",
+        image: "imgurl",
+        time: "1:22",
+        Artist: 1
+    },
+    {
+        name: "song1",
+        url: "songurl",
+        image: "imgurl",
+        time: "1:22",
+        Artist: 1
+    },
+    {
+        name: "song1",
+        url: "songurl",
+        image: "imgurl",
+        time: "1:22",
+        Artist: 1
+    },
+    {
+        name: "song1",
+        url: "songurl",
+        image: "imgurl",
+        time: "1:22",
+        Artist: 1
+    },
+    {
+        name: "song1",
+        url: "songurl",
+        image: "imgurl",
+        time: "1:22",
+        Artist: 1
+    }
+]
